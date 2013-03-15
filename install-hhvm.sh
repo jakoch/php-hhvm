8384c24575a16019aca57eaf41164e5f020d0e23
jakoch <jakoch@web.de>
2013-03-16 00:39:17 +0100

    honey badger's give a shit - http://www.youtube.com/watch?v=4r7wHMg5Yjg

---
+++ install-hhvm.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+#
+# Building and installing HHVM
+#
+# https://github.com/facebook/hiphop-php/wiki/Building-and-installing-HHVM-on-Ubuntu-12.04
+#
+
+# Install all package dependencies
+function install_dependencies() {
+    echo -n "Update & Install package dependencies."
+	apt-get update -y
+	apt-get install -y git-core cmake g++ libboost-dev libmysqlclient-dev \
+	  libxml2-dev libmcrypt-dev libicu-dev openssl build-essential binutils-dev \
+	  libcap-dev libgd2-xpm-dev zlib1g-dev libtbb-dev libonig-dev libpcre3-dev \
+	  autoconf libtool libcurl4-openssl-dev libboost-system-dev \
+	  libboost-program-options-dev libboost-filesystem-dev wget memcached \
+	  libreadline-dev libncurses-dev libmemcached-dev libbz2-dev \
+	  libc-client2007e-dev php5-mcrypt php5-imagick libgoogle-perftools-dev \
+	  libcloog-ppl0 libelf-dev libdwarf-dev libunwind7-dev subversion
+)
+
+function get_hiphop_source() {
+	mkdir dev
+	cd dev
+	git clone git://github.com/facebook/hiphop-php.git
+	cd hiphop-php
+	export CMAKE_PREFIX_PATH=`/bin/pwd`/..
+	export HPHP_HOME=`/bin/pwd`
+	export HPHP_LIB=`/bin/pwd`/bin
+	export USE_HHVM=1
+	cd ..
+}
+
+# libevent
+function install_libevent() {
+    echo "Installing libevent."
+	git clone git://github.com/libevent/libevent.git
+	cd libevent
+	git checkout release-1.4.14b-stable
+	cat ../hiphop-php/hphp/third_party/libevent-1.4.14.fb-changes.diff | patch -p1
+	./autogen.sh
+	./configure --prefix=$CMAKE_PREFIX_PATH
+	make && make install
+    cd ..
+}
+
+# libCurl
+function install_libcurl() {
+    echo "Install curl."
+    git clone git://github.com/bagder/curl.git
+	cd curl
+	./buildconf
+	./configure --prefix=$CMAKE_PREFIX_PATH
+	make && make install
+	cd ..
+}
+
+# google glog
+function install_googleglog() {
+	svn checkout http://google-glog.googlecode.com/svn/trunk/ google-glog
+	cd google-glog
+	./configure --prefix=$CMAKE_PREFIX_PATH
+	make && make install
+	cd ..
+}
+
+# jemalloc
+function install_jemalloc() {
+	wget http://www.canonware.com/download/jemalloc/jemalloc-3.0.0.tar.bz2
+	tar xjvf jemalloc-3.0.0.tar.bz2
+	cd jemalloc-3.0.0
+	./configure --prefix=$CMAKE_PREFIX_PATH
+	make && make install
+	cd ..
+}
+
+function build() {
+	cd hiphop-php
+	git submodule init
+	git submodule update
+	export HPHP_HOME=`pwd`
+	export HPHP_LIB=`pwd`/bin
+	cmake .
+	make
+}
+
+function install() {
+    install_dependencies
+	get_hiphop_source
+	install_libevent
+    install_libcurl
+    install_googleglog
+    install_jemalloc   
+	build
+}
+
+install
+
+ln -fs ${CMAKE_PREFIX_PATH}/hiphop-php/src/hphp/hphp /usr/bin/hphp
+ln -fs ${CMAKE_PREFIX_PATH}/hiphop-php/src/hhvm/hhvm /usr/bin/hhvm
+
+## Success
+echo "HipHop-PHP is now installed!"
+echo " > /usr/bin/hphp"
+echo " > /usr/bin/hhvm"
+exit 0
