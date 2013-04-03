26e8c43d933cd751b31efb4e612279527ed692fc
jakoch <jakoch@web.de>
2013-04-04 00:49:01 +0200

    color me blind ... use correct path to hhvm on examples

--- install-hhvm.sh
+++ install-hhvm.sh
@@ -193,7 +193,12 @@ ${CMAKE_PREFIX_PATH}/hiphop-php/hphp/hhvm/hhvm --help
 ## Getting started with Hello-World
 echo -e "<?php\n echo \"Hello Hiphop-PHP!\";\n?>" > hello.php
 
+# Example of the Server Mode
 #${CMAKE_PREFIX_PATH}/hiphop-php/hphp/hhvm/hhvm -m server -p 8123 ./
 #lynx -source http://127.0.0.1:8123/hello.php
 
-hhvm -f hello.php
+# Example of executing specified file
+${CMAKE_PREFIX_PATH}/hiphop-php/hphp/hhvm/hhvm --file hello.php
+
+# Example of parsing the specified file and dumping the AST
+${CMAKE_PREFIX_PATH}/hiphop-php/hphp/hhvm/hhvm --parse hello.php
