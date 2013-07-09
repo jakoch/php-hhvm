f78d503ea285f7d827963ad5fb4b1e90f5f14fb6
jakoch <jakoch@web.de>
2013-07-09 18:24:31 +0200

    oops copy paste error

--- example.php
+++ example.php
@@ -7,7 +7,7 @@
  * Global Vars are not forbidden, but BAD!
  */
 $global_variable_a = 'Hello';
-echo $global_variable_a . ' Static Analyzer!';;
+echo $global_variable_a . ' Static Analyzer!';
 
 /**
  * Dynamic Variable Access
@@ -15,8 +15,8 @@ echo $global_variable_a . ' Static Analyzer!';;
 $$global_variable_a = ' World!'; // takes the value of $global_variable_a and uses it as the new variable name (= new var $Hello)
 
 // all outputs are the same: "Hello World!"
-echo "$a ${$a}";
-echo "$a $Hello";
-echo "$a $hello"; // variable names are not case sensitve
+echo "$global_variable_a ${$global_variable_a}";
+echo "$global_variable_a $Hello";
+echo "$global_variable_a $hello"; // variable names are not case sensitve
 
 ?>
