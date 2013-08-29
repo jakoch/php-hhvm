81810e4cd199fbfc732a49516562e77a9096b4dd
jakoch <jakoch@web.de>
2013-08-29 18:04:07 +0200

    missed a sudo

--- install-hhvm.sh
+++ install-hhvm.sh
@@ -48,7 +48,7 @@ function install_dependencies() {
 
     sudo apt-get update -y
 
-    apt-get -t experimental install libmemcached-dev
+    sudo apt-get -t experimental install libmemcached-dev
 
     echo -e "\e[1;32m> Done.\e[0m"
     echo
